2d9a44d3e44f8ee71e74907f088d2a5620d8db4c
AlbertoWindows <albertopimentel04@gmail.com>
2020-08-16 18:40:52 -0500

    DONE

--- src/App.js
+++ src/App.js
@@ -1,24 +1,152 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+import InfoBox from "./InfoBox";
+import Map from "./Map";
+import Table from "./Table";
+import "./App.css";
+import { sortData, prettyPrintStat } from "./util";
+import LineGraph from "./LineGraph";
+import "leaflet/dist/leaflet.css";
+import numeral from "numeral";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("Global");
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([]);
+  const [casesType, setCasesType] = useState("cases");
+
+  //?Primera Vez que recargas la pagina trae los datos del mundo
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
+
+  //state = how to write a variable in REACT
+
+  // https://disease.sh/v3/covid-19/countries
+
+  //USEEFFECT =Runs a piece of code based on a given condition
+
+  useEffect(() => {
+    //asyn -> send a request, wait for it, so somethig with the info
+
+    const getContriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country, // United States, United Kingdom
+            value: country.countryInfo.iso2, // UK, USA, FR
+          }));
+          const sortedData = sortData(data);
+          setTableData(sortedData);
+          setMapCountries(data);
+          setCountries(countries);
+        });
+    };
+    getContriesData();
+  }, []);
+
+  //?cuando seleccionas un pais o worldwide, trae los datos del pais o del mundo
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+
+    const url =
+      countryCode === "Global"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        //All the data from the country response
+        setCountry(countryCode);
+        setCountryInfo(data);
+
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(4);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              onChange={onCountryChange}
+              value={country}
+            >
+              {/*Loop through all the countries and show a drop down list of the ooptions */}
+              <MenuItem value="Global">Global</MenuItem>
+
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox
+            isRed
+            active={casesType === "cases"}
+            onClick={(e) => setCasesType("cases")}
+            title="Casos de Coronavirus (hoy)"
+            cases={numeral(countryInfo.todayCases).format("0,0")}
+            total={prettyPrintStat(countryInfo.cases)}
+          />
+          <InfoBox
+            active={casesType === "recovered"}
+            onClick={(e) => setCasesType("recovered")}
+            title="Recuperados (hoy) "
+            cases={numeral(countryInfo.todayRecovered).format("0,0")}
+            total={prettyPrintStat(countryInfo.recovered)}
+          />
+          <InfoBox
+            isRed
+            active={casesType === "deaths"}
+            onClick={(e) => setCasesType("deaths")}
+            title="Muertes (hoy)"
+            cases={numeral(countryInfo.todayDeaths).format("0,0")}
+            total={prettyPrintStat(countryInfo.deaths)}
+          />
+        </div>
+        {/*Map*/}
+        <Map
+          casesType={casesType}
+          countries={mapCountries}
+          center={mapCenter}
+          zoom={mapZoom}
+        />
+      </div>
+
+      <Card className="app__right">
+        <CardContent>
+          <h3>Casos en vivo por Pais</h3>
+          {/*Table */}
+          <Table countries={tableData} />
+          <h3 className="app__graphTitle"> Worlwide New {casesType}</h3>
+          {/*Graph*/}
+          <LineGraph className="app__graph" casesType={casesType} />
+        </CardContent>
+      </Card>
     </div>
   );
 }
